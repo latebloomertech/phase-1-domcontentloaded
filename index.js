7a9bd5eb130a647e617de8387352d5f988bf2c0a
Dan Baechtold <latebloomertech@gmail.com>
2022-01-17 17:03:34 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Your code goes here
+const init = () => {
+ const para =  document.getElementById('text')
+ para.textContent = 'This is really cool!';
+ para.style.color = 'red';
+}
+
+document.addEventListener('DOMContentLoaded', init)
